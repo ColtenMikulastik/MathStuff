aeebadbc07b35bea9a929382a86d90dae149a12f
Colten <mikulastikc@gmail.com>
2023-01-18 22:12:45 -0600

    working on new quadratic calculator

---
+++ QuadraticFormula/__main__.py
@@ -0,0 +1,16 @@
+
+
+def calculate_quadratic(a_var, b_var, c_var):
+    """calculates the roots for a quadratic formula"""
+    print("a: " + str(a_var)) 
+    print("b: " + str(b_var)) 
+    print("c: " + str(c_var)) 
+
+
+def main():
+    print( "hello!")
+    calculate_quadratic(1,2,3)
+
+
+if __name__ == "__main__":
+    main()

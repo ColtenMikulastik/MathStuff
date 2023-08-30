7d79727732d48423b0245c170c14e455f9329587
Colten <mikulastikc@gmail.com>
2023-08-30 13:19:36 -0500

    adding something about kinetic fomulas for physics

---
+++ Kenetic_Forms/__main__.py
@@ -0,0 +1,25 @@
+import math
+
+def calc():
+    """ gonna get the info that we need and calculate with the appropriate formula"""
+
+    # goign to get input from the user, assuming no string is unknown
+    init_vel = input("what is your initial velocity: ")
+    fin_vel = input("what is your final velocity: ")
+    dif_time = input("what is your change in time: ")
+    accel = input("what is your acceleration: ")
+    dif_pos = input("what is your displacement: ")
+    
+    # this will probably take some time that I don't really have rn
+    if (init_vel == "" || fin_vel == "") && 
+    
+
+
+    if test == "":
+        print("nothing")
+    else:
+        print(str(test))
+
+
+if __name__ == "__main__":
+    calc()

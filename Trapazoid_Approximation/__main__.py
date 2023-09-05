dc6b2f8a4650c971acfe16777017f18c4f6a0455
ColtenMikulastik <mikulastikc@gmail.com>
2023-09-05 11:15:26 -0500

    working on the input, thinking about how to get function

--- Trapazoid_Approximation/__main__.py
+++ Trapazoid_Approximation/__main__.py
@@ -1,10 +1,20 @@
 
 
+
+def trapazoid_approx(n_var, func):
+
+    # get the information that you want to approximate
+    start_a = input("What is your starting pos (a): ")
+    stop_b = input("What is your stopping pos (b): ")
+    
+
+
 def main():
     """ this function will drive the trapazoidal approximation function """
     print("test")
-
-
+    func = input("please give your function in some format: ")
+    n_var = input("please give your n variable (how accurate your approximation is): ")
+    trapazoid_approx(n_var, func)
 
 
 if __name__ == "__main__":

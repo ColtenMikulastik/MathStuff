6c5f663c876b3a0bf766e6e6d0d177773482f8f1
Colten <mikulastikc@gmail.com>
2023-09-05 15:35:53 -0500

    learned about eval function and implemented it

--- Trapazoid_Approximation/__main__.py
+++ Trapazoid_Approximation/__main__.py
@@ -1,11 +1,16 @@
 
 
-
 def trapazoid_approx(n_var, func):
-
+    """ using your n and func to calculate a trapazoid approximation """
     # get the information that you want to approximate
-    start_a = input("What is your starting pos (a): ")
-    stop_b = input("What is your stopping pos (b): ")
+    start_a = int(input("What is your starting pos (a): "))
+    stop_b = int(input("What is your stopping pos (b): "))
+    # omg just found out that this function exists :^O eval()
+
+    # loop from a to b, with i
+    for x in range(start_a, stop_b + 1):
+        # replace x in the loop with those values
+        print(str(eval(func)))
     
 
 

61cdf4fecf2b8643cde5a07a83c7ebc4411f5d3c
ColtenMikulastik <mikulastikc@gmail.com>
2025-01-21 16:49:22 -0600

    moved thing to a function and added other base functionality

--- Number_Systems/main.py
+++ Number_Systems/main.py
@@ -1,16 +1,30 @@
 
-thing1 = [ 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1 ]
-# attempting thing in octal
-thing = [ 1, 1, 1, 1, 0, 1 ]
 
-# Convert from a given base number system to decimal
-sum = 0
-invert = 0
-base = 2 
-for i in range((len(thing) - 1), -1, -1):
-    radix = base**invert
-    digit = thing[i]
-    sum = sum + (radix * digit)
-    invert = invert + 1
+def convert_to_dec( base, word ):
+    """ Convert from a given base number system to decimal """
+    # base should be an int, and work should be a list of numbers
+    sum = 0
+    invert = 0
+    for i in range((len(word) - 1), -1, -1):
+        radix = base**invert
+        digit = word[i]
+        sum = sum + (radix * digit)
+        invert = invert + 1
 
-print(sum)
+    print(sum)
+    return sum
+
+
+def main():
+    print("hello")
+    print("passing to the convert to dec function")
+    
+    thing1 = [ 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1 ]
+    thing2 = [ 3, 2, 0 ]
+    convert_to_dec(4, thing2)
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()

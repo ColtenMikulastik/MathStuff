15cd2016f505d47ef4dded8afc211281104fbaa2
ColtenMikulastik <mikulastikc@gmail.com>
2025-01-29 14:19:44 -0600

    small changes to palendrome

---
+++ Palendrome/paledrometest.py
@@ -0,0 +1,28 @@
+
+
+
+def palendrome():
+    """ largest palidrome made from the product of two 3-digit numbers """
+    # think this might be a bad way to solve but brute force
+    # need every possible combination of two three digit numbers
+    for i in range(100, 999):
+        for j in range(100, 999):
+            print(i * j)
+            # need to check if the answer is same forward and back
+
+
+
+def is_same_backwards(in_str):
+    i = len(number)
+    for number in in_int:
+        print(number)
+
+
+def main():
+    palendrome()
+
+
+
+
+if __name__ == "__main__":
+    main()

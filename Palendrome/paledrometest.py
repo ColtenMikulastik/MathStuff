5e475b35d0a3f8a717112dfb80f7568e3b14d641
ColtenMikulastik <mikulastikc@gmail.com>
2025-01-30 07:54:48 -0600

    largest palendrome solution added

--- Palendrome/paledrometest.py
+++ Palendrome/paledrometest.py
@@ -5,17 +5,32 @@ def palendrome():
     """ largest palidrome made from the product of two 3-digit numbers """
     # think this might be a bad way to solve but brute force
     # need every possible combination of two three digit numbers
+    list_o_nums = list()
     for i in range(100, 999):
         for j in range(100, 999):
-            print(i * j)
+            if (is_same_backwards(str(i * j))):
+                print(i * j)
+                list_o_nums.append(i * j)
             # need to check if the answer is same forward and back
+
+    list_o_nums.sort()
+    print(list_o_nums)
 
 
 
 def is_same_backwards(in_str):
-    i = len(number)
-    for number in in_int:
-        print(number)
+    """ takes an input string and returns true if it is a palendrome """
+    i = len(in_str) - 1
+    # loop through the string and return false if any don't match
+    for j in in_str:
+        # if they are not equal return false
+        if in_str[i] != j:
+            return False
+        # step the inverse iterator
+        i = i -1
+
+    # if the function gets here then return true
+    return True
 
 
 def main():

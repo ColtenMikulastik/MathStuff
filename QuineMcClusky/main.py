7cca077fcf74b7df0450bf35916ac0cf15e9881e
Colten Mikulastik <mikulastikc@gmail.com>
2025-03-16 17:57:56 -0500

    code quality stuff fixed, got my liniter to work

--- QuineMcClusky/main.py
+++ QuineMcClusky/main.py
@@ -9,7 +9,6 @@ def hyphenate(l_binary_num, r_binary_num, data_size):
             return_me.append(r_binary_num[i])
 
     return return_me
-            
 
 
 def is_one_off(l_binary_num, r_binary_num, data_size):
@@ -25,6 +24,7 @@ def is_one_off(l_binary_num, r_binary_num, data_size):
         return 1
     else:
         return 0
+
 
 def one_amount(binary_num):
     """ returns number of ones """
@@ -45,15 +45,15 @@ def qmmethod(in_list, data_size):
         bin_list.append(i)
     groups = list(list())
 
-    for i in range(0,data_size):
+    for i in range(0, data_size):
         temp_list = list()
         for j in bin_list:
             if one_amount(j) == i:
                 temp_list.append(j)
         groups.append(temp_list)
     print(groups)
-    
-    group_combinations = list(list()) 
+
+    group_combinations = list(list())
     i = 0
     while i < (len(groups) - 1):
         temp_list = list()
@@ -63,21 +63,24 @@ def qmmethod(in_list, data_size):
                     temp_list.append(hyphenate(j, k, data_size))
         group_combinations.append(temp_list)
         i += 1
+
+    counter = 0
     for i in group_combinations:
+        print(str(counter) + ", " + str(counter + 1))
         for j in i:
             print(j)
         print("================")
-
-
-
+        counter += 1
 
 
 def main():
-    minterms = [ 0,2,6,7,8,10,11,12,13,14,16,18,19,29,30 ]
+    minterms = [0, 2, 6, 7, 8, 10, 11, 12, 13, 14, 16, 18, 19, 29, 30]
+    dont_cares = [4, 9, 21]
+    minterms = minterms + dont_cares
+    minterms.sort()
     qmmethod(minterms, 5)
-    print(is_one_off("011", "110",3))
+    print(is_one_off("011", "110", 3))
 
 
-
-
-main()
+if __name__ == "__main__":
+    main()
